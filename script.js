5eb7ad7f47e39e8de42535973c12c29d3c066dc4
sirisha akurathi <akurathi.sirisha@gmail.com>
2023-09-24 15:47:39 +0530

    Add existing files to Git

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const dice = document.getElementById("dice");
+const adviceNum = document.getElementById("advice-number");
+const adviceText = document.querySelector(".advice-text");
+
+
+window.onload = showQuote;
+
+
+dice.addEventListener("click", function(){
+    showQuote();
+});
+
+
+function showQuote(){
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then((data) => data.slip)
+    .then((data) => {
+        adviceNum.textContent = data.id;
+        adviceText.innerHTML = data.advice;
+    })
+    .catch((error) => {
+        alert(`Error ${error}`);
+    });
+}
